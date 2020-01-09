58a42fdb30361ba4982485b99ebe0770285b0107
Swastik <s.agarwal-2@student.tudelft.nl>
2020-01-10 00:51:02 +0100

    progress i guess

---
+++ public/javascript/function.js
@@ -0,0 +1,8 @@
+var element = document.getElementById("board");
+var check = checkElement;
+element.addEventListener("click", check);
+
+function checkElement(e)
+{
+    alert("hello world "+e.getElementById);
+}
