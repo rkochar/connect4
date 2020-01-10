32823043bb21762320cf466d52a82323073dfbe8
Swastik <s.agarwal-2@student.tudelft.nl>
2020-01-10 15:28:59 +0100

    additions

--- public/javascript/function.js
+++ public/javascript/function.js
@@ -1,8 +1,22 @@
 var element = document.getElementById("board");
 var check = checkElement;
 element.addEventListener("click", check);
-
+var gameBoard3 = [
+	[0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0],
+]
 function checkElement(e)
 {
-    alert("hello world "+e.getElementById);
+    var pop = e.toElement.id;
+    pop = parseInt(pop,10) + 35;
+    while(pop>41)
+    {
+        pop = pop - 7;
+    }
+    alert(pop);
+    var element = document.getElementById("board");
 }
