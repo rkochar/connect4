07925f15a7958b647dd4115958e0456b35848339
Swastik <s.agarwal-2@student.tudelft.nl>
2020-01-11 02:20:29 +0100

    almost done

--- public/javascript/function.js
+++ public/javascript/function.js
@@ -1,14 +1,7 @@
 var element = document.getElementById("board");
 var check = checkElement;
 element.addEventListener("click", check);
-var gameBoard3 = [
-	[0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0],
-]
+var gameBoard3 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 function checkElement(e)
 {
     var pop = e.toElement.id;
@@ -17,6 +10,29 @@ function checkElement(e)
     {
         pop = pop - 7;
     }
-    alert(pop);
-    var element = document.getElementById("board");
+    while(gameBoard3[pop]==1)
+    {
+        pop = pop -7;
+    }
+    gameBoard3[pop] = 1;
+    var button = document.getElementById(pop+"");
+    if(document.getElementById("current-player").innerHTML.localeCompare("Player 1")==0)
+    {
+    button.style.background = '#bf2121';
+    alert("next player");
+    document.getElementById("current-player").innerHTML = "Player 2";
+    
+    var element = document.getElementById("other-player");
+    element.innerHTML="Player 1";
+    }
+    else{
+    button.style.background = '#fffb1f';
+    alert("next player");
+    document.getElementById("current-player").innerHTML = "Player 1";
+    
+    var element = document.getElementById("other-player");
+    element.innerHTML="Player 2";
+    }
+    
+
 }
