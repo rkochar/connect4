f184cde4aabf2c2b57b7954b66523aa98a773863
Swastik <s.agarwal-2@student.tudelft.nl>
2020-01-13 17:16:44 +0100

    win conditions

--- app.js
+++ app.js
@@ -1,8 +1,137 @@
 var express = require("express");
 var http = require("http");
+var websocket = require("ws");
 
 var port = process.argv[2];
 var app = express();
 
 app.use(express.static(__dirname + "/public"));
-http.createServer(app).listen(port);  
+http.createServer(app).listen(port);
+
+
+
+var indexRouter = require("./routes/index");
+// var messages = require("./public/javascripts/messages");
+
+// var gameStatus = require("./statTracker");
+// var Game = require("./game");
+
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+app.get("/play", indexRouter);
+const wss = new websocket.Server({ server });
+var websockets = {};
+var server = http.createServer(app);
+let gamesStarted = 0
+let gamesOngoing = function () {
+    let glen = games.length
+    let count = 0
+    for (let i = 0; i < glen; i++) {
+        if (games[i].state === "ongoing")
+            count++;
+    }
+    return count
+}
+let gamesCompleted = 0
+var games = [];
+
+wss.on("connection", function (ws) {
+    console.log("Connection Made");
+
+    ws.on("close", function () {
+        let gameId
+        let player
+        for (let index = 0; index < games.length; index++) {
+            const game = games[index];
+            if (game.playerA === ws) {
+                gameId = index;
+                player = "A"
+                break
+            } else if (game.playerB === ws) {
+                gameId = index;
+                player = "B"
+                break
+            }
+        }
+        if (games[gameId].state !== "ended") {
+            if (player === "A" && games[gameId].playerB) {
+                games[gameId].playerB.send(JSON.stringify({
+                    type: "abort"
+                }))
+            }
+            else if (player === "B" && games[gameId].playerA) {
+                games[gameId].playerA.send(JSON.stringify({
+                    type: "abort"
+                }))
+            }
+            gamesCompleted++
+            games[gameId].state = "ended"
+        }
+    })
+
+    ws.on("message", function (json_msg) {
+        let message = JSON.parse(json_msg)
+
+        switch (message.type) {
+            case "playerJoin":
+                let gameFound
+                for (let gameIndex = 0; gameIndex < games.length; gameIndex++) {
+                    let game = games[gameIndex]
+                    if (game.state === "waiting") {
+                        gameFound = game
+                        if (!game.playerA) {
+                            game.playerA = ws
+                            ws.send(JSON.stringify({
+                                type: "playerAdded",
+                                data: {
+                                    gameId: gameIndex,
+                                    playerType: "A"
+                                }
+                            }))
+                        }
+                        else if (!game.playerB) {
+                            game.playerB = ws
+                            ws.send(JSON.stringify({
+                                type: "playerAdded",
+                                data: {
+                                    gameId: gameIndex,
+                                    playerType: "B"
+                                }
+                            }))
+                            game.state = "ongoing"
+
+                            game.playerA.send(JSON.stringify({
+                                type: "gameStart"
+                            }))
+                            game.playerB.send(JSON.stringify({
+                                type: "gameStart"
+                            }))
+
+                            gamesStarted++;
+                        }
+                    }
+                }
+                if (!gameFound) {
+                    games.push({
+                        state: "waiting",
+                        playerA: ws,
+                        playerB: null,
+                        playerAPos: 1,
+                        playerBPos: 1,
+                        currentTurn: "A"
+                    })
+                    ws.send(JSON.stringify({
+                        type: "playerAdded",
+                        data: {
+                            gameId: games.length - 1,
+                            playerType: "A"
+                        }
+                    }))
+                }
+                break
+
+        }
+    })
+});
--- public/javascript/function.js
+++ public/javascript/function.js
@@ -1,74 +1,90 @@
 var element = document.getElementById("board");
 var check = checkElement;
+var ws = new WebSocket('ws://localhost:3000');
+ws.onopen = function (event) {
+    console.log('Connection is open ...');
+};
 element.addEventListener("click", check);
 var gameBoard3 = [
-[0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0], 
-[0, 0, 0, 0, 0, 0, 0], 
-[0, 0, 0, 0, 0, 0, 0], 
-[0, 0, 0, 0, 0, 0, 0], 
-[, 0, 0, 0, 0, 0, 0]]
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [, 0, 0, 0, 0, 0, 0]]
 function checkElement(e) {
     var pop = e.toElement.id;
-    var type = e.toElement.type+"";
-    if(type.localeCompare("button")==0){    
-    pop = '5'+pop.charAt(1);
-    while (gameBoard3[pop.charAt(0)][pop.charAt(1)] == 1 || gameBoard3[pop.charAt(0)][pop.charAt(1)] == 2) {
-        var fn = pop.charAt(0);
-        var sn = pop.charAt(1);
-        pop = fn -1+""+sn;
+    var type = e.toElement.type + "";
+    if (type.localeCompare("button") == 0) {
+        pop = '5' + pop.charAt(1);
+        while (gameBoard3[pop.charAt(0)][pop.charAt(1)] == 1 || gameBoard3[pop.charAt(0)][pop.charAt(1)] == 2) {
+            var fn = pop.charAt(0);
+            var sn = pop.charAt(1);
+            pop = fn - 1 + "" + sn;
+        }
+        if (document.getElementById("current-player").innerHTML.localeCompare("Player 1") == 0) {
+            gameBoard3[pop.charAt(0)][pop.charAt(1)] = 1;
+            var button = document.getElementById(pop + "");
+            button.style.background = '#bf2121';
+            alert("next player");
+            document.getElementById("current-player").innerHTML = "Player 2";
+            if(win())
+            {
+                alert("u won");
+            }
+            var element = document.getElementById("other-player");
+            element.innerHTML = "Player 1";
+        }
+        else {
+            gameBoard3[pop.charAt(0)][pop.charAt(1)] = 2;
+            var button = document.getElementById(pop + "");
+            button.style.background = '#fffb1f';
+            alert("next player");
+            document.getElementById("current-player").innerHTML = "Player 1";
+            if(win())
+            {
+                alert("u won");
+            }
+            var element = document.getElementById("other-player");
+            element.innerHTML = "Player 2";
+        }
+
     }
-     if (document.getElementById("current-player").innerHTML.localeCompare("Player 1") == 0) {
-        gameBoard3[pop.charAt(0)][pop.charAt(1)] = 1;
-        var button = document.getElementById(pop + "");
-        button.style.background = '#bf2121';
-        alert("next player");
-        document.getElementById("current-player").innerHTML = "Player 2";
-
-        var element = document.getElementById("other-player");
-        element.innerHTML = "Player 1";
-    }
-    else {
-        gameBoard3[pop.charAt(0)][pop.charAt(1)] = 2;
-        var button = document.getElementById(pop + "");
-        button.style.background = '#fffb1f';
-        alert("next player");
-        document.getElementById("current-player").innerHTML = "Player 1";
-
-        var element = document.getElementById("other-player");
-        element.innerHTML = "Player 2";
-    }
-
-    }
-    // var pop = e.toElement.id;
-    // pop = parseInt(pop, 10) + 35;
-    // while (pop > 41) {
-    //     pop = pop - 7;
-    // }
-    // while (gameBoard3[pop] == 1 || gameBoard3[pop] == 2) {
-    //     pop = pop - 7;
-    // }
-
-    // if (document.getElementById("current-player").innerHTML.localeCompare("Player 1") == 0) {
-    //     gameBoard3[pop] = 1;
-    //     var button = document.getElementById(pop + "");
-    //     button.style.background = '#bf2121';
-    //     alert("next player");
-    //     document.getElementById("current-player").innerHTML = "Player 2";
-
-    //     var element = document.getElementById("other-player");
-    //     element.innerHTML = "Player 1";
-    // }
-    // else {
-    //     gameBoard3[pop] = 2;
-    //     var button = document.getElementById(pop + "");
-    //     button.style.background = '#fffb1f';
-    //     alert("next player");
-    //     document.getElementById("current-player").innerHTML = "Player 1";
-
-    //     var element = document.getElementById("other-player");
-    //     element.innerHTML = "Player 2";
-    // }
-
-
 }
+
+function win() {
+    var height = 6;
+    var width = 7;
+    var n = 0;
+    var player
+    for ( r = 0; r < height; r++) { // iterate rows, bottom to top
+        for ( c = 0; c < width; c++) { // iterate columns, left to right
+            player = gameBoard3[r][c];
+            if (player == 0)
+                continue; // don't check empty slots
+            if (c + 3 < width &&
+                player == gameBoard3[r][c + 1] && // look right
+                player == gameBoard3[r][c + 2] &&
+                player == gameBoard3[r][c + 3])
+                return true;
+            if (r + 3 < height) {
+                if (player == gameBoard3[r + 1][c] && // look up
+                    player == gameBoard3[r + 2][c] &&
+                    player == gameBoard3[r + 3][c])
+                    return true;
+                if (c + 3 < width &&
+                    player == gameBoard3[r + 1][c + 1] && // look up & right
+                    player == gameBoard3[r + 2][c + 2] &&
+                    player == gameBoard3[r + 3][c + 3])
+                    return true;
+                if (c - 3 >= 0 &&
+                    player == gameBoard3[r + 1][c - 1] && // look up & left
+                    player == gameBoard3[r + 2][c - 2] &&
+                    player == gameBoard3[r + 3][c - 3])
+                    return true;
+            }
+        }
+    }
+    return false;
+}
+
